a6cefc75434f3598b08b3715eed8497f42ac37cb
Caroline <wanguic49@gmail.com>
2023-10-13 14:09:47 +0300

    few changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule,Routes } from '@angular/router';
+import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ChatComponent } from './chat/chat.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 const routes:Routes = [
   {path:'',pathMatch: 'full',redirectTo:'home'},
@@ -21,7 +24,9 @@ const routes:Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatTooltipModule
   ],
   exports:[RouterModule],
   providers: [],
